f93b85b17cb9cfbcc89a7e145f76d447d9f9d460
Vivek43017 <121903548+Vivek43017@users.noreply.github.com>
2023-01-03 17:02:12 -0500

    Add files via upload
    if jira has a user to add to a group and the transition is set to appear as a UI button, as use case if the user wants to be part of jira test group and later assigned to a role or assigned group permission to test then please use this script.

---
+++ testuseradd.groovy
@@ -0,0 +1,8 @@
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.user.ApplicationUser
+
+def groupManager = ComponentAccessor.getGroupManager()
+def user = ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser()
+def groupName = “jiratest-group"
+
+groupManager.addUserToGroup(user, groupName)
