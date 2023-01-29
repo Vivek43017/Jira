9152c62c7745822fcb8c05eab67817f6d6b5f142
Vivek43017 <121903548+Vivek43017@users.noreply.github.com>
2023-01-29 03:12:44 -0500

    Create jira_lm.py

---
+++ jira_lm.py
@@ -0,0 +1,25 @@
+from jira import JIRA
+import numpy as np
+from keras.models import load_model
+
+# Connect to Jira
+jira = JIRA(basic_auth=('username', 'password'), options={'server': 'https://your-jira-url.com'})
+
+# Load your deep learning model
+model = load_model('model.h5')
+
+# Get the data for a specific Jira issue
+issue = jira.issue('ISSUE-123')
+issue_data = {
+    'summary': issue.fields.summary,
+    'description': issue.fields.description,
+    'priority': issue.fields.priority.name,
+    'status': issue.fields.status.name
+}
+
+# Use the deep learning model to make a prediction
+input_data = np.array([issue_data['summary'], issue_data['description'], issue_data['priority'], issue_data['status']])
+prediction = model.predict(input_data)
+
+# Update the Jira issue with the prediction
+jira.add_custom_field(issue, 'Prediction', prediction)
